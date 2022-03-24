13b971d34159d9fcadc2ffc1ab83cd6350b4fcd4
xenholic <zenobiaholic@gmail.com>
2022-03-24 19:27:05 +0700

    Andri

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-const data = {
-    username: ['andri', 'fahri', 'fitra'],
-    password: [123456, 123458, 112233]
-}
-function Login(data) {
-  let result = [];
-  for (let i = 0; i < data.username.length; i++) {
-      let id = data.username[i]
-      for (let j = 0; j < data.password.length; j++) {
-          let pass = data.password[j]
-          }
-          
-      }
-      
-  }
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-  console.log(Login('andri',123456))
-//   var done = 0;
-//   var username = document.login.username.value;
-//   username = username.toLowerCase();
-//   var password = document.login.password.value;
-//   password = password.toLowerCase();
-//   if (username == "Andri" && password == "123456") {
-//     alert("selamat Login berhasil ");
-//   } else if (done == 0) {
-//     console.log("Invalid login!");
-//   }
-// }
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+
+$("#search-icon").click(function() {
+  $(".nav").toggleClass("search");
+  $(".nav").toggleClass("no-search");
+  $(".search-input").toggleClass("search-active");
+});
+
+$('.menu-toggle').click(function(){
+   $(".nav").toggleClass("mobile-nav");
+   $(this).toggleClass("is-active");
+});
